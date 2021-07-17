f102cb645e41948fb25891143a495622e90e4fff
Matheus <matheus_crema@hotmail.com>
2021-07-17 15:17:05 -0300

    Correção no arquivo script.js

--- Landing-Page/js/script.js
+++ Landing-Page/js/script.js
@@ -24,25 +24,25 @@ form.addEventListener('submit', (e) => {
 })
 
 // -- Contador -- //
-var countDownDate = new Date("July 23, 2021 17:00:00").getTime(); // Data até onde o contador vai
+let countDownDate = new Date("July 23, 2021 17:00:00").getTime(); // Data até onde o contador vai
 
-var x = setInterval(function() { // Aumenta o contador a cada 1 segundo
+let x = setInterval(function() { // Aumenta o contador a cada 1 segundo
 
-    var now = new Date().getTime(); // Get todays date and time
+    let now = new Date().getTime(); // Pega o dia e a data de hoje(referente ao dia que está sendo executado)
 
-    var distance = countDownDate - now; // Calcula quanto tempo há de diferença do dia atual até o final da contagem.
+    let distance = countDownDate - now; // Calcula quanto tempo há de diferença do dia atual até o final da contagem.
 
     // Calculo para conseguir os Dias, Horas, Minutos e Segundos
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     document.getElementById("dday").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`; // Mostra o resultado do contador onde estiver o id="dday"
 
     // Quando acaba a contagem escreve a mensagem que estiver aqui
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("dday").innerHTML = "A Black Friday Tech Store já começou!";
+        document.getElementById("dday").innerHTML = "A Black Friday Geek Store já começou! Aproveite as promoções.";
     }
 }, 1000);
